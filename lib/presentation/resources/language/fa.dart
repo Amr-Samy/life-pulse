9f28e42593350c0977f422028efbe3c9ffa5798f
Abdalrahman Bakr <abdalrahmanbakr.dev@gmail.com>
2025-09-24 22:20:23 +0300

    delete unused files in translate

--- lib/presentation/resources/language/fa.dart
+++
@@ -1,337 +0,0 @@
-import '../strings_manager.dart';
-
-Map<String,String> fa = {
-  AppStrings.onBoardingTitle1 : 'ما بهترین دوره‌های آموزشی و معلمان عالی را ارائه می‌دهیم!',
-  AppStrings.onBoardingTitle2 : 'در هر زمان و هر مکان به راحتی و آسانی یاد بگیرید',
-  AppStrings.onBoardingTitle3 : "بیایید مهارت‌های شما را با مبین بهبود بخشیم!",
-  AppStrings.skip : "رد کردن",
-  AppStrings.next : "بعدی",
-  AppStrings.gGetStarted : "شروع کنید",
-  //:::::::::  Auth ::::::::: \\
-  AppStrings.let : "بگذارید وارد شوید",
-  AppStrings.cwf : "ادامه با فیسبوک",
-  AppStrings.cwg : "ادامه با گوگل",
-  AppStrings.cwa : "ادامه با اپل",
-  AppStrings.or : "یا",
-  AppStrings.signWPassword : "ورود با رمز عبور",
-  AppStrings.dontHaveAccount : "حساب کاربری ندارید؟",
-  AppStrings.signUp : "ثبت نام",
-  //:::::::::  Sign ::::::::: \\
-  AppStrings.ltya : "به حساب کاربری خود وارد شوید",
-  AppStrings.email : "ایمیل",
-  AppStrings.password : "رمز عبور",
-  AppStrings.rememberMe : "مرا به خاطر بسپار",
-  AppStrings.signIn : "ورود",
-  AppStrings.forgot : "رمز عبور را فراموش کرده‌اید؟",
-  AppStrings.continueWith : "یا ادامه با",
-  AppStrings.createAccount : "حساب کاربری ایجاد کنید",
-  AppStrings.alreadyHave : "قبلاً حساب کاربری دارید؟",
-
-  //:::::::::  Account Setup ::::::::: \\
-  AppStrings.fillYourProfile : "پروفایل خود را تکمیل کنید",
-  AppStrings.fullName : "نام کامل",
-  AppStrings.nickname : "نام مستعار",
-  AppStrings.dateOfBirth : "تاریخ تولد (سال-ماه-روز)",
-  AppStrings.phoneNumber : "شماره تلفن",
-  AppStrings.gender : "جنسیت",
-  AppStrings.continueOption : "ادامه",
-  AppStrings.congratulations : "تبریک می‌گوییم!",
-  AppStrings.accountReady : "حساب شما آماده استفاده است. طی چند ثانیه به صفحه اصلی هدایت خواهید شد.",
-
-  //:::::::::  Account Setup ::::::::: \\
-  AppStrings.forgotPassword : "فراموشی رمز عبور",
-  AppStrings.selectResetMethod : "انتخاب کنید که از چه روشی برای بازیابی رمز عبور استفاده کنیم",
-  AppStrings.viaSMS : "از طریق پیامک:",
-  AppStrings.viaEmail : "از طریق ایمیل:",
-  AppStrings.codeSendTo : "کد به آدرس ارسال شد ",
-  AppStrings.resend : "ارسال مجدد کد در ",
-  AppStrings.second : " ثانیه",
-  AppStrings.createYourNewPassword : "رمز عبور جدید ایجاد کنید",
-  AppStrings.createNewPassword : "رمز عبور جدید ایجاد کنید",
-  AppStrings.confirmNewPassword : "تأیید رمز عبور جدید",
-
-  //:::::::::  Home & Action Menu ::::::::: \\
-  AppStrings.goodMorning : "صبح بخیر 👋",
-  AppStrings.goodAfternoon  : "عصر بخیر 🌙",
-  AppStrings.search : "جستجو",
-  AppStrings.todaySpecial : "پیشنهاد ویژه امروز",
-  AppStrings.getDiscount : """در هر سفارش دوره تخفیف دریافت کنید! فقط امروز معتبر است!""",
-  AppStrings.topTutors : "برترین معلمان",
-  AppStrings.seeAll : "مشاهده همه",
-  AppStrings.students : "دانشجویان",
-  AppStrings.notifications : "اعلان‌ها",
-  AppStrings.today : "امروز",
-  AppStrings.yesterday : "دیروز",
-  AppStrings.myBookmark : "نشان‌گذاری‌های من",
-  AppStrings.removeBookmark : "از نشان‌گذاری‌ها حذف شود؟",
-  AppStrings.cancel : "لغو",
-  AppStrings.close : "بستن",
-  AppStrings.confirmRemove : "بله، حذف شود",
-  AppStrings. courses : " دوره‌ها",
-  AppStrings.filter : "فیلتر",
-  AppStrings.mainCategories : "دسته‌بندی‌های اصلی:",
-  AppStrings.academic : "آکادمیک (مبین)",
-  AppStrings.generalPurpose  : "عمومی",
-  AppStrings.subCategories : "زیردسته‌ها:",
-  AppStrings.levels : "سطوح",
-  AppStrings.price : "قیمت:",
-  AppStrings.rating : "امتیاز:",
-  AppStrings.videoDurations : "مدت زمان ویدیوها:",
-  AppStrings.apply : "اعمال",
-  AppStrings.recent : "اخیر",
-  AppStrings.clearAll : "پاک کردن همه",
-  AppStrings.resultsFor  : "نتایج برای ",
-  AppStrings. found : " یافت شد",
-  AppStrings.notFound : "پیدا نشد",
-  AppStrings.category : "دسته‌بندی",
-  AppStrings.reset : "بازنشانی",
-  AppStrings.tutors : "معلمان",
-  AppStrings.searchNotFound : "متأسفیم، کلمه کلیدی که وارد کرده‌اید یافت نشد. لطفاً دوباره بررسی کنید یا با کلمه کلیدی دیگری جستجو کنید.",
-  AppStrings.mostPopularCourses : "محبوب‌ترین دوره‌ها",
-
-  //:::::::::  Course Details & Payment/Enroll ::::::::: \\
-  AppStrings.hours : "ساعت",
-  AppStrings.certificate : "گواهینامه",
-  AppStrings.about : "درباره",
-  AppStrings.lessons : "درس‌ها",
-  AppStrings.lesson : "درس",
-  AppStrings.tutor : "معلم",
-  AppStrings.aboutCourse : "درباره دوره",
-  AppStrings.tools : "ابزارها",
-  AppStrings.enrollCourse  : "ثبت نام در دوره",
-  AppStrings.reEnroll  : "ثبت نام مجدد",
-  AppStrings.reviews : "نظرات",
-  AppStrings.message : "پیام",
-  AppStrings.writeReview  : "می‌توانید نظر خود را اینجا بنویسید",
-  AppStrings.characters : "کاراکتر",
-  AppStrings.shareCourse : "اشتراک گذاری دوره",
-  AppStrings.viewReceipt : "مشاهده رسید",
-  AppStrings.reviewCourse : "بررسی دوره",
-  AppStrings.selectPayment : "روش پرداخت را انتخاب کنید",
-  AppStrings.selectPaymentDescription : "روش پرداختی که می‌خواهید استفاده کنید را انتخاب کنید",
-  AppStrings.applyPromotion : "اعمال کد تخفیف",
-  AppStrings.applyPromotionDescription : "برای قیمت کمتر، از کد تخفیف استفاده کنید.",
-  AppStrings.enterPromoCode  : "کد تخفیف را وارد کنید",
-  AppStrings.cardHolder : "دارنده کارت",
-  AppStrings.cardNumber : "شماره کارت",
-  AppStrings.expiryDate : "تاریخ انقضا",
-  AppStrings.pay : "پرداخت",
-
-  //:::::::::  Course Details & Payment/Enroll ::::::::: \\
-  AppStrings.ongoing : "در حال انجام",
-  AppStrings.completed : "تکمیل شده",
-  AppStrings.hrs : "ساعت",
-  AppStrings.min : "دقیقه",
-  AppStrings.minute : "دقیقه",
-  AppStrings.curriculum : "برنامه درسی",
-  AppStrings.certificates : "گواهینامه‌ها",
-  AppStrings.startCourseAgain : "شروع مجدد دوره",
-  AppStrings.downloadCertificate : "دانلود گواهینامه",
-  AppStrings.courseCompleted : "دوره تکمیل شد!",
-  AppStrings.leaveReview : """لطفاً برای دوره خود نظر بگذارید.""",
-  AppStrings.continueCourse : "ادامه دوره",
-  AppStrings.video : "ویدیو",
-  AppStrings.materials : "مواد",
-  AppStrings.practices : "تمرینات",
-  AppStrings.interaction : "تعامل",
-  AppStrings.quiz : "آزمون",
-  AppStrings.sectionComplete : "بخش تکمیل شد",
-  AppStrings.nextSection : "بخش بعدی",
-  AppStrings.quizDescription : "این یک روش ساده برای شرکت در آزمون MCQ آنلاین است. این سوالات MCQ برای تمام موضوعاتی که یاد گرفته‌اید بسیار مهم است",
-  AppStrings.startQuiz : "شروع آزمون",
-  AppStrings.question : "سوال",
-  AppStrings.tryAgain : "دوباره امتحان کنید",
-  AppStrings.yourScore : "امتیاز شما",
-  AppStrings.sorry : "متأسفیم",
-
-  //:::::::::  Inbox/Messenger (Chats & Calls) ::::::::: \\
-  AppStrings.inbox : "صندوق ورودی",
-  AppStrings.chats : "چت‌ها",
-  AppStrings.sendMessage : "پیام...",
-
-  //:::::::::  Transactions ::::::::: \\
-  AppStrings.transactions : "تراکنش‌ها",
-  AppStrings.receipt : "رسید",
-  AppStrings.isPaid : "پرداخت شده",
-  AppStrings.unpaid : "پرداخت نشده",
-  AppStrings.enrolled : "ثبت نام شده",
-
-  //:::::::::  Profile & Settings ::::::::: \\
-  AppStrings.profile : "پروفایل",
-  AppStrings.editProfile : "ویرایش پروفایل",
-  AppStrings.payment : "پرداخت",
-  AppStrings.security : "امنیت",
-  AppStrings.language : "زبان",
-  AppStrings.darkMode : "حالت تیره",
-  AppStrings.privacyPolicy : "سیاست حفظ حریم خصوصی",
-  AppStrings.helpCenter : "مرکز راهنمایی",
-  AppStrings.logout : "خروج",
-  AppStrings.update : "به‌روزرسانی",
-  AppStrings.faceID : "Face ID",
-  AppStrings.biometricID : "Biometric ID",
-  AppStrings.changePIN : "تغییر PIN",
-  AppStrings.changePassword : "تغییر رمز عبور",
-  AppStrings.suggested : "پیشنهادی",
-  AppStrings.faq : "سوالات متداول",
-  AppStrings.contactUs : "تماس با ما",
-  AppStrings.customerService : "پشتیبانی مشتریان",
-  AppStrings.whatsApp : "واتس‌اپ",
-  AppStrings.website : "وبسایت",
-  AppStrings.facebook : "فیسبوک",
-  AppStrings.x : "X",
-  AppStrings.instagram : "اینستاگرام",
-  AppStrings.confirmLogout : "آیا مطمئن هستید که می‌خواهید خارج شوید؟",
-  AppStrings.yesLogout : "بله، خارج شوید",
-
-  //Missing
-  AppStrings.male : "مرد",
-  AppStrings.female : "زن",
-  AppStrings.selectGender : "جنسیت را انتخاب کنید",
-  AppStrings.wait : "صبر کنید",
-  AppStrings.otpErrorM : "باید یک دقیقه صبر کنید قبل از ارسال مجدد کد",
-  AppStrings.shareReceipt : "اشتراک گذاری رسید",
-  AppStrings.downloadReceipt : "دانلود رسید",
-  AppStrings.print : "چاپ",
-  AppStrings.section1 : "بخش 1",
-  AppStrings.section2 : "بخش 2",
-  AppStrings.section3 : "بخش 3",
-  AppStrings.home :  "خانه",
-  AppStrings.myCourses : "دوره‌های من",
-  AppStrings.mcqHead :  "یکی از این گزینه‌ها را انتخاب کنید",
-  AppStrings.all :  "همه",
-  AppStrings.suggestedCourses :  "دوره‌های پیشنهادی",
-  AppStrings.readMore :  "بیشتر بخوانید ...",
-  AppStrings.readLess :  "... کمتر بخوانید",
-  AppStrings.allLevels :  "همه سطوح",
-  AppStrings.beginners :  "مبتدی",
-  AppStrings.academicLevel :  "آکادمیک",
-  AppStrings.intermediate :  "متوسط",
-  AppStrings.expert :  "حرفه‌ای",
-  AppStrings.paid :  "پولی",
-  AppStrings.free :  "رایگان",
-  AppStrings.andAbove :  "و بالاتر",
-  AppStrings.filters :  "فیلترها",
-  AppStrings.clear :  "پاک کردن",
-  AppStrings.confirmPayment :  "تأیید پرداخت",
-  AppStrings.enterPIN :  "برای تأیید پرداخت PIN خود را وارد کنید",
-  AppStrings.enrollSuccess :  "ثبت نام در دوره موفقیت‌آمیز بود!",
-  AppStrings.paymentSuccess :  "شما با موفقیت پرداخت را انجام داده و در دوره ثبت نام کردید.",
-  AppStrings.viewCourse :  "مشاهده دوره",
-  AppStrings.confirm :  "تأیید",
-  AppStrings.cvvError :  "لطفاً یک CVV معتبر وارد کنید",
-  AppStrings.expireDateError :  "لطفاً یک تاریخ معتبر وارد کنید",
-  AppStrings.cardNumberError :  "لطفاً یک شماره معتبر وارد کنید",
-  AppStrings.course :  "دوره",
-  AppStrings.name :  "نام",
-  AppStrings.phone :  "تلفن",
-  AppStrings.country :  "کشور",
-  AppStrings.paymentMethod :  "روش‌های پرداخت",
-  AppStrings.promoCode :  "کد تخفیف",
-  AppStrings.date :  "تاریخ",
-  AppStrings.transactionId :  "شناسه تراکنش",
-  AppStrings.transactionIdCopied :  "شناسه تراکنش در کلیپ‌بورد کپی شد",
-  AppStrings.founds :  "یافت شد",
-  AppStrings.review :  "بررسی",
-  AppStrings.suggest :  "پیشنهاد به دوست",
-  AppStrings.writeAReview :  "نظر بنویسید",
-  AppStrings.inviteFriends :  "دعوت از دوستان",
-  AppStrings.off :  "تخفیف",
-  AppStrings.invited :  "دعوت شده",
-  AppStrings.invite :  "دعوت",
-  AppStrings.error :  "خطا",
-  AppStrings.note :  "یادداشت",
-  AppStrings.checkMail :  "لطفاً ایمیل خود را برای تأیید بررسی کنید",
-  AppStrings.shareExperience :  "تجربه خود را به اشتراک بگذارید تا به دیگران کمک کنید!",
-  AppStrings.expired :  "منقضی شده",
-  AppStrings.status :  "وضعیت",
-  AppStrings.noPracticesAvailable :  "هیچ تمرینی موجود نیست",
-  AppStrings.previous :  "قبلی",
-  AppStrings.submit :  "ارسال",
-  AppStrings.empty :  "خالی",
-  AppStrings.logInHint :  "برای ورود و دسترسی به این بخش ضربه بزنید",
-  AppStrings.chatEmptyText :  "برای شروع چت جدید پیام ارسال کنید",
-  AppStrings.newChat :  "چت جدید",
-  AppStrings.inReview :  "در حال بررسی",
-  AppStrings.objectives :  "اهداف",
-  AppStrings.requirements :  "پیشنیازها",
-  AppStrings.learningTargets :  "اهداف یادگیری",
-  AppStrings.learningPractices :  "تمرینات یادگیری",
-  AppStrings.materialsEmptyState :  "هیچ ماده‌ای برای این درس وجود ندارد",
-  AppStrings.invalidPdf :  "فایل PDF نامعتبر",
-  AppStrings.showMaterials :  "نمایش مواد",
-  AppStrings.showInteraction :  "نمایش تعامل",
-  AppStrings.showPractices :  "نمایش تمرینات",
-  AppStrings.nextLesson :  "درس بعدی",
-  AppStrings.noTitle :  "بدون عنوان",
-  AppStrings.noData :  "داده‌ای موجود نیست",
-  AppStrings.check :  "بررسی",
-  AppStrings.correctAnswer :  "پاسخ صحیح",
-  AppStrings.correct :  "صحیح",
-  AppStrings.inCorrect :  "نادرست. دوباره امتحان کنید!",
-  AppStrings.play :  "پخش",
-  AppStrings.typeAnswer :  "پاسخ خود را اینجا تایپ کنید...",
-  AppStrings.emptyReviews :  "اکنون دوره را دریافت کنید و اولین نفری باشید که این دوره را بررسی می‌کند",
-  AppStrings.fullNameRequired :  "نام کامل الزامی است",
-  AppStrings.placementTest :  "آزمون تعیین سطح",
-  AppStrings.redoTest :  'انجام مجدد آزمون',
-  AppStrings.suggestedCoursesForYou :  'دوره‌های پیشنهادی برای شما',
-  AppStrings.testCompleted :  'آزمون با موفقیت تکمیل شد!',
-  AppStrings.excellent :  'عالی',
-  AppStrings.good :  'خوب',
-  AppStrings.needsImprovement :  'نیاز به بهبود دارد',
-  AppStrings.retry :  'تلاش مجدد',
-  AppStrings.noQuestionsAvailable :  'هیچ سوالی موجود نیست',
-  AppStrings.of :  'از',
-  AppStrings.yourAnswer :  'پاسخ شما',
-  AppStrings.noContent :  'هیچ محتوایی موجود نیست',
-  AppStrings.unsupportedQuestion :  'نوع سؤال پشتیبانی نمی‌شود',
-  AppStrings.nextPractice :  'تمرین بعدی',
-  //API
-  AppStrings.apiError: "هنگام پردازش درخواست شما خطایی رخ داد.",
-  AppStrings.invalidToken: "توکن نامعتبر. لطفاً دوباره وارد شوید.",
-  AppStrings.forbidden: "ممنوع.",
-  AppStrings.connectionTimeOut: "زمان اتصال به پایان رسید، لطفاً دوباره تلاش کنید.",
-  AppStrings.sendTimeOut: "زمان ارسال به پایان رسید، لطفاً دوباره تلاش کنید.",
-  AppStrings.receiveTimeOut: "زمان دریافت به پایان رسید، لطفاً دوباره تلاش کنید.",
-  AppStrings.invalidRequest: "درخواست نامعتبر.",
-  AppStrings.internalError: "خطای داخلی سرور رخ داد، لطفاً بعداً دوباره تلاش کنید.",
-  AppStrings.conflictError: "تعارض رخ داد.",
-  AppStrings.unauthorizedAccess: "دسترسی غیرمجاز.",
-  AppStrings.notFoundError: "منبع درخواستی یافت نشد.",
-  AppStrings.noInternetError: "اتصال به اینترنت تشخیص داده نشد، لطفاً دوباره تلاش کنید.",
-  AppStrings.unKnownError: "خطای ناشناخته.",
-  AppStrings.singInError: "نام کاربری یا رمز عبور اشتباه است",
-  AppStrings.emailPasswordRequired: "ایمیل و رمز عبور الزامی هستند",
-  AppStrings.streamNotStarted: "پخش زنده هنوز شروع نشده است",
-  AppStrings.noDescription: "هیچ توضیحی موجود نیست",
-  AppStrings.unknownFile: "فایل ناشناخته",
-  AppStrings.minRead: "دقیقه خواندن",
-  AppStrings.live: "زنده",
-  AppStrings.record: "ضبط شده",
-  AppStrings.downloadFailed: "دانلود ناموفق بود",
-  AppStrings.showText: "نمایش متن",
-  AppStrings.liveTime: "زمان پخش زنده به وقت مکه: ",
-  AppStrings.quizCompleted: "آزمون تکمیل شد!",
-  AppStrings.percentage: "درصد: ",
-  AppStrings.untitled: "بدون عنوان",
-  AppStrings.noOnGoing: "شما هیچ دوره در حال انجامی ندارید",
-  AppStrings.noCompleted: "شما هیچ دوره تکمیل شده‌ای ندارید",
-  AppStrings.noExpired: "شما هیچ دوره منقضی شده‌ای ندارید",
-  AppStrings.noBookmarks: "شما هیچ دوره نشان‌گذاری شده‌ای ندارید",
-  AppStrings.noRequirements: "هیچ پیشنیازی وجود ندارد",
-  AppStrings.paidSuccessfully: "پرداخت با موفقیت انجام شد و در انتظار تأیید ادمین است",
-  AppStrings.successfullyCompleted: "با موفقیت تکمیل شد",
-  AppStrings.subscriptionConfirmed: "اشتراک با موفقیت تأیید شد",
-  AppStrings.noCourses: "هیچ دوره‌ای موجود نیست",
-  AppStrings.noCourseReviews: "اکنون دوره را دریافت کنید و اولین نفری باشید که این دوره را بررسی می‌کند",
-  AppStrings.failedToLoadQuestions: "بارگیری سوالات ناموفق بود",
-  AppStrings.failedToSubmit: "ارسال آزمون ناموفق بود",
-  AppStrings.oops: "اوه! مشکلی پیش آمد!",
-  AppStrings.weEncounteredAnError: "ما با خطایی مواجه شدیم و تیم مهندسی ما را مطلع کرده‌ایم. بابت این مشکل عذرخواهی می‌کنیم.",
-  AppStrings.otherLanguages : "زبان‌های دیگر",
-  ///
-  AppStrings.introduction: "معرفی",
-  AppStrings.theLesson: "درس",
-  AppStrings.exercises: "تمرین‌ها",
-
-};
